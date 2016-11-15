4e9e55987d3c21ce99d50969f227e5055fe6bd3c
Babu Shanmugam <bschanmu@redhat.com>
2016-11-15 12:05:49 +0000

    Input DB listen IP address for the ovn-northd
    When ovn-northd is started, along with it ovsdb servers for the southbound
    and northbound databases are also started. Without additional options for the
    ovn-northd service, the DB servers will be listenting on 0.0.0.0.
    
    This patch creates a systemd environment file that will create the db listen
    options for the northd service. It also sets the other options
    db-nb-create-insecure-remote and db-sb-create-insecure-remote which
    are required to start the ovsdb-server's  listening on TCP
    connections. Please see this commit [1] for more details.
    
    [1] -
    https://github.com/openvswitch/ovs/commit/84d0ca5d00fe01b29163236d48fa0f9105687149
    
    Co-authored-by: Numan Siddique <nusiddiq@redhat.com>
    Change-Id: I99b1c3e0fe89b763be81db9b8c1c3b257be42e6e

--- manifests/northd.pp
+++ manifests/northd.pp
@@ -3,9 +3,22 @@
 #
 # installs ovn package starts the ovn-northd service
 #
-class ovn::northd() {
+# [*dbs_listen_ip*]
+#   The IP-Address where OVN DBs should be listening
+#   Defaults to '0.0.0.0'
+#
+class ovn::northd($dbs_listen_ip = '0.0.0.0') {
   include ::ovn::params
   include ::vswitch::ovs
+
+  if $::osfamily == 'RedHat' {
+    augeas { 'sysconfig-ovn-northd':
+      context =>  '/files/etc/sysconfig/ovn-northd',
+      changes =>  "set OVN_NORTHD_OPTS '\"--db-nb-addr=${dbs_listen_ip} --db-sb-addr=${dbs_listen_ip} \
+--db-nb-create-insecure-remote=yes --db-sb-create-insecure-remote=yes\"'",
+      before  =>  Service['northd'],
+    }
+  }
 
   service { 'northd':
     ensure    => true,
