e0881669b08446747c0533af78be67bb42251574
Matthew J. Black <mjblack@gmail.com>
2017-01-18 11:38:19 -0500

    Northd class include the vswitch::ovs class
    The vswitch::ovs class handles installing and starting the
    openvswitch-switch service. The northd requires ovs to be
    able to communicate with the ovs instances via geneve
    tunnel.
    
    Change-Id: I72ae31f49c3103085e42cee97690042b51fde578

--- manifests/northd.pp
+++ manifests/northd.pp
@@ -4,19 +4,22 @@
 # installs ovn package starts the ovn-northd service
 #
 class ovn::northd() {
-    include ::ovn::params
+  include ::ovn::params
+  include ::vswitch::ovs
 
-    service { 'northd':
-        ensure    => true,
-        enable    => true,
-        name      => $::ovn::params::ovn_northd_service_name,
-        hasstatus => $::ovn::params::ovn_northd_service_status,
-        pattern   => $::ovn::params::ovn_northd_service_pattern,
-    }
+  service { 'northd':
+    ensure    => true,
+    enable    => true,
+    name      => $::ovn::params::ovn_northd_service_name,
+    hasstatus => $::ovn::params::ovn_northd_service_status,
+    pattern   => $::ovn::params::ovn_northd_service_pattern,
+    require   => Service['openvswitch']
+  }
 
-    package { $::ovn::params::ovn_northd_package_name:
-        ensure => present,
-        name   => $::ovn::params::ovn_northd_package_name,
-        before => Service['northd']
-    }
+  package { $::ovn::params::ovn_northd_package_name:
+    ensure  => present,
+    name    => $::ovn::params::ovn_northd_package_name,
+    before  => Service['northd'],
+    require => Package[$::vswitch::params::ovs_package_name]
+  }
 }
