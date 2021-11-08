openstack/puppet-ovn

commit c60fdb9540bc5dfc60cf05d56ad9d8fe45164343

Deprecate duplicated implementation for hw-offload option
Currently the hw-offload option is managed by two modules,
puppet-vswitch and puppet-ovn. Because this option is generic and
valid for both ovs and ovn this change deprecate the parameter from
puppet-ovn.

Depends-on: https://review.opendev.org/779802
Change-Id: I4b402c58cc3c39cd2650d91d12d607fe0171e1d0

manifests/controller.pp
@@ -33,12 +33,6 @@
 #   (optional) Name of the integration bridge.
 #   Defaults to 'br-int'
 #
-# [*enable_hw_offload*]
-#   (optional) Configure OVS to use
-#   Hardware Offload. This feature is
-#   supported from ovs 2.8.0.
-#   Defaults to False.
-#
 # [*mac_table_size*]
 #  Set the mac table size for the provider bridges if defined in ovn_bridge_mappings
 #  Defaults to 50000
@@ -87,6 +81,14 @@
 #  vlan type logical switch.
 #  Defaults to empty list
 #
+# DEPRECATED PARAMETERS
+#
+# [*enable_hw_offload*]
+#   (optional) Configure OVS to use
+#   Hardware Offload. This feature is
+#   supported from ovs 2.8.0.
+#   Defaults to undef.
+#
 class ovn::controller(
   $ovn_remote,
   $ovn_encap_ip,
@@ -95,7 +97,6 @@ class ovn::controller(
   $bridge_interface_mappings   = [],
   $hostname                    = $::fqdn,
   $ovn_bridge                  = 'br-int',
-  $enable_hw_offload           = false,
   $mac_table_size              = 50000,
   $datapath_type               = $::os_service_default,
   $enable_dpdk                 = false,
@@ -104,6 +105,8 @@ class ovn::controller(
   $ovn_transport_zones         = [],
   $enable_ovn_match_northd     = false,
   $ovn_chassis_mac_map         = [],
+  # DEPRECATED PARAMETERS
+  $enable_hw_offload           = undef,
 ) {
 
   include ovn::params
@@ -187,9 +190,15 @@ class ovn::controller(
     $tz_items = {}
   }
 
-  if $enable_hw_offload {
-    $hw_offload = { 'other_config:hw-offload' => { 'value' => bool2str($enable_hw_offload) } }
-  }else {
+  if $enable_hw_offload != undef {
+    warning('The ovn::controller::enable_hw_offload parmaeter is deperecated. \
+Use the same parameter of vswitch::ovs or vswitch::dpdk')
+    if $enable_hw_offload {
+      $hw_offload = { 'other_config:hw-offload' => { 'value' => bool2str($enable_hw_offload) } }
+    } else {
+      $hw_offload = {}
+    }
+  } else {
     $hw_offload = {}
   }
 

releasenotes/notes/deprecate-enable_hw_offload-51f91d8d09f559f6.yaml
@@ -1,0 +1,5 @@
+---
+deprecations:
+  - |
+    The ``ovn::controller::enable_hw_offload`` parameter has been deprecated.
+    Use the same parameter of ``vswitch::ovs`` or ``vswitch::dpdk`` instead.

commit d2c29451c4c88fc97f08f78dc07642150ef6e463

Add ovn-monitor-all option to ovn-controller
With monitor-all option, all ovn-controllers
do not enable conditional monitoring there
by reducing the load on the Southbound ovsdb-server.

Conflicts:
	manifests/controller.pp

(wallaby -> victoria)
Conflicts:
	manifests/controller.pp

Resolved conflict caused by c60fdb9540bc5dfc60cf05d56ad9d8fe45164343

Partial-Bug: #1936781
Signed-off-by: Kamil Sambor <ksambor@redhat.com>
Change-Id: Ia78c0873e7ac6488b3048ae8475cc87ec40022bb
(cherry picked from commit 7a1bb545198d8c864c619b30f4bc13417d764bbd)
(cherry picked from commit 9879556556e9cb2bc9139799e93da5feb60124b3)

manifests/controller.pp
@@ -80,6 +80,12 @@
 #  vlan type logical switch.
 #  Defaults to empty list
 #
+# [*ovn_monitor_all*]
+#  (optional) A boolean value that tells if ovn-controller should monitor all
+#  records of tables in ovs-database. If set to false, it will conditionally
+#  monitor the records that is needed in the current chassis.
+#  Default to false (keep the original behavior)
+#
 class ovn::controller(
   $ovn_remote,
   $ovn_encap_ip,
@@ -96,6 +102,7 @@ class ovn::controller(
   $ovn_openflow_probe_interval = 60,
   $ovn_transport_zones         = [],
   $ovn_chassis_mac_map         = [],
+  $ovn_monitor_all             = false,
 ) {
 
   include ovn::params
@@ -138,6 +145,7 @@ class ovn::controller(
     'external_ids:ovn-bridge'                   => { 'value' => $ovn_bridge },
     'external_ids:ovn-remote-probe-interval'    => { 'value' => "${ovn_remote_probe_interval}" },
     'external_ids:ovn-openflow-probe-interval'  => { 'value' => "${ovn_openflow_probe_interval}" },
+    'external_ids:ovn-monitor-all'              => { 'value' => "${ovn_monitor_all}" },
   }
 
   if !empty($ovn_chassis_mac_map) {

releasenotes/notes/ovn-monitor-all-001cafc946130936.yaml
@@ -1,0 +1,4 @@
+---
+features:
+  - |
+    The new ``ovn::controller::ovn_monitor_all`` parameter has been added.

spec/classes/ovn_controller_spec.rb
@@ -14,6 +14,7 @@ describe 'ovn::controller' do
       :mac_table_size              => 20000,
       :ovn_remote_probe_interval   => 30000,
       :ovn_openflow_probe_interval => 8,
+      :ovn_monitor_all             => true,
       :ovn_transport_zones         => ['tz1'],
       :ovn_chassis_mac_map         => ['physnet1:aa:bb:cc:dd:ee:ff',
                                        'physnet2:bb:aa:cc:dd:ee:ff']
@@ -74,6 +75,10 @@ describe 'ovn::controller' do
 
       is_expected.to contain_vs_config('external_ids:ovn-openflow-probe-interval').with(
         :value   => params[:ovn_openflow_probe_interval],
+      )
+
+      is_expected.to contain_vs_config('external_ids:ovn-monitor-all').with(
+        :value   => params[:ovn_monitor_all],
       )
 
       is_expected.to contain_vs_config('external_ids:ovn-transport-zones').with(