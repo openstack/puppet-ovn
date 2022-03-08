f0a84faff3d05c717925dfa879b644cbabfbfd57
Takashi Kajinami <tkajinam@redhat.com>
2022-03-08 09:22:32 +0900

    remove unicode literal from code
    All strings are considered as unicode string from Python 3.
    
    This patch drops the explicit unicode literal (u'...')
    appearances from the unicode strings.
    
    Change-Id: I8a366377074dee83069d2489b12e3af373dccc48
    Co-Authored-By: LiZekun <2954674728@qq.com>

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -45,8 +45,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'puppet-ovn Release Notes'
-copyright = u'2017, Puppet OpenStack Developers'
+project = 'puppet-ovn Release Notes'
+copyright = '2017, Puppet OpenStack Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -194,8 +194,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'puppet-ovnReleaseNotes.tex', u'puppet-ovn Release Notes Documentation',
-   u'2017, Puppet OpenStack Developers', 'manual'),
+  ('index', 'puppet-ovnReleaseNotes.tex', 'puppet-ovn Release Notes Documentation',
+   '2017, Puppet OpenStack Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -224,8 +224,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'puppet-ovnreleasenotes', u'puppet-ovn Release Notes Documentation',
-     [u'2017, Puppet OpenStack Developers'], 1)
+    ('index', 'puppet-ovnreleasenotes', 'puppet-ovn Release Notes Documentation',
+     ['2017, Puppet OpenStack Developers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -238,8 +238,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'puppet-ovnReleaseNotes', u'puppet-ovn Release Notes Documentation',
-   u'2017, Puppet OpenStack Developers', 'puppet-ovnReleaseNotes', 'One line description of project.',
+  ('index', 'puppet-ovnReleaseNotes', 'puppet-ovn Release Notes Documentation',
+   '2017, Puppet OpenStack Developers', 'puppet-ovnReleaseNotes', 'One line description of project.',
    'Miscellaneous'),
 ]
 
