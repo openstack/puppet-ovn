223ce32f85507b2995b449ca6cf085590551cb64
Takashi Kajinami <tkajinam@redhat.com>
2022-08-23 23:35:45 +0900

    Add support for external_ids:ovn-encap-tos
    ... which is available since v21.12.0[1].
    
    [1] https://github.com/ovn-org/ovn/commit/9f2eb5cdb6d67fc8c7e6f477bc4bdc94cfbca028
    
    Conflicts:
    	spec/classes/ovn_controller_spec.rb
    
    (xena to wallaby)
    Conflicts:
    	manifests/controller.pp
    
    Change-Id: I33931579b15a1629007dbe3a3d48433739b0ce56
    (cherry picked from commit 054a60c360f6ef9a06c146131f502444b8a9f0d0)
    (cherry picked from commit 3c14a1757f3f3645bea15855a4f1ddd97f11b8b2)
    (cherry picked from commit d8a40a0ef01cd527401c1b16f46ac7b52f46a009)

--- manifests/controller.pp
+++ manifests/controller.pp
@@ -17,6 +17,10 @@
 #   (Required) IP address of the hypervisor(in which this module is installed) to which
 #   the other controllers would use to create a tunnel to this controller
 #
+# [*ovn_encap_tos*]
+#   (Optional) The value to be applied to OVN tunnel interface's option:tos.
+#   Defaults to undef
+#
 # [*ovn_bridge_mappings*]
 #   (optional) List of <ovn-network-name>:<bridge-name>
 #   Defaults to empty list
@@ -99,6 +103,7 @@ class ovn::controller(
   $ovn_remote,
   $ovn_encap_ip,
   $ovn_encap_type              = 'geneve',
+  $ovn_encap_tos               = undef,
   $ovn_bridge_mappings         = [],
   $bridge_interface_mappings   = [],
   $hostname                    = $::fqdn,
@@ -157,6 +162,16 @@ class ovn::controller(
     'external_ids:ovn-remote-probe-interval'    => { 'value' => "${ovn_remote_probe_interval}" },
     'external_ids:ovn-openflow-probe-interval'  => { 'value' => "${ovn_openflow_probe_interval}" },
     'external_ids:ovn-monitor-all'              => { 'value' => "${ovn_monitor_all}" },
+  }
+
+  if $ovn_encap_tos {
+    $encap_tos = {
+      'external_ids:ovn-encap-tos' => { 'value' => $ovn_encap_tos }
+    }
+  } else {
+    $encap_tos = {
+      'external_ids:ovn-encap-tos' => { 'ensure' => 'absent' }
+    }
   }
 
   if !empty($ovn_chassis_mac_map) {
@@ -219,7 +234,10 @@ Use the same parameter of vswitch::ovs or vswitch::dpdk')
   $ovn_match_northd = {
     'external_ids:ovn-match-northd-version' => { 'value' => bool2str($enable_ovn_match_northd) }
   }
-  create_resources('vs_config', merge($config_items, $chassis_mac_map, $bridge_items, $tz_items, $hw_offload, $datapath_config, $ovn_match_northd))
+  create_resources(
+    'vs_config',
+    merge($config_items, $encap_tos, $chassis_mac_map, $bridge_items, $tz_items, $hw_offload, $datapath_config, $ovn_match_northd)
+  )
   Service['openvswitch'] -> Vs_config<||> -> Service['controller']
 
   if !empty($ovn_bridge_mappings) {
